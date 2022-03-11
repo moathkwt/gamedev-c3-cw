ae64fc28d44e2294fbcbce256fd044c7addd1b5d
moathkwt <100304331+moathkwt@users.noreply.github.com>
2022-03-11 10:15:00 +0300

    New maze game

--- maze game/Assets/playermovementscript.cs
+++ maze game/Assets/playermovementscript.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playermovementscript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // player movement 
+
+    void OnMouseDrag(){
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 playerPos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = playerPos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter2D(Collision2D other){
+        SceneManager.LoadScene("lv1");
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag== "Finish")
+        {
+            SceneManager.LoadScene("jumpScare");
+        }
+        else
+        {
+            SceneManager.LoadScene("lv2");
+        }
     }
 }
